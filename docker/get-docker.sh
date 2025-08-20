a8c33adc1897a79b41cedc6078b641ec1fa25d89
kaosay <58497583+kaosay@users.noreply.github.com>
2025-08-20 10:05:54 +0800

    Update get-docker.sh
    install

--- docker/get-docker.sh
+++ docker/get-docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # Offline install docker
+
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
+apt install docker-ce
